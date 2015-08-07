0e7cabfc708c1fb916106bd8f41b21e74554f74e
Brady Wied <brady@bswtechconsulting.com>
2015-08-07 17:59:35 -0600

    Try and do this but it's not working yet

--- app/controllers/dummy_controller.rb
+++ app/controllers/dummy_controller.rb
@@ -1,4 +1,15 @@
+import 'org.hibernate.SessionFactory'
+import 'org.hibernate.boot.registry.StandardServiceRegistryBuilder'
+import 'org.hibernate.cfg.Configuration'
+
 class DummyController < ApplicationController
   def index
+    sf = get_session_factory
+  end
+  
+  def get_session_factory
+    @@session_factory ||= begin
+      Configuration.new.configure('config/hibernate.cfg.xml').buildSessionFactory(StandardServiceRegistryBuilder.new().build())
+    end
   end
 end
