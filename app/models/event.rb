71dec7da92cb716097a7d128821b30bdc4a8f228
Brady Wied <brady@bswtechconsulting.com>
2015-08-10 09:37:11 -0600

    Tried to get the session factory to build by adding getters/setters but could not get it working

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,31 @@
-class Event
-  attr_accessor :id
-  attr_accessor :title
-  attr_accessor :date
+module Bsw
+  class Event
+    attr_accessor :id
+    attr_accessor :title
+    attr_accessor :date
+    
+    def getId
+      @id
+    end
+    
+    def setId(id)
+      @id = id
+    end
+    
+    def getTitle
+      @title
+    end
+    
+    def setTitle(title)
+      @title = title
+    end
+    
+    def getDate
+      @date
+    end
+    
+    def setDate(date)
+      @date = date
+    end
+  end
 end
