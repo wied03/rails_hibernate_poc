ba66594c329dfb912ed7c75baf473e9f023631cc
Brady Wied <brady@bswtechconsulting.com>
2016-03-26 14:00:03 -0600

    conventions

--- app/models/bsw/event.rb
+++ app/models/bsw/event.rb
@@ -3,27 +3,27 @@ module Bsw
     attr_accessor :id
     attr_accessor :title
     attr_accessor :date
-    
+
     def getId
       @id
     end
-    
+
     def setId(id)
       @id = id
     end
-    
+
     def getTitle
       @title
     end
-    
+
     def setTitle(title)
       @title = title
     end
-    
+
     def getDate
       @date
     end
-    
+
     def setDate(date)
       @date = date
     end
