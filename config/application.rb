7681ddeb5d876d5801237f04a4780d4054c75f8c
Brady Wied <brady@bswtechconsulting.com>
2016-03-26 14:02:32 -0600

    Get a pure java version of this working

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ require "action_view/railtie"
 require "sprockets/railtie"
 require "rails/test_unit/railtie"
 $CLASSPATH << 'config/hibernate.cfg.xml'
+$CLASSPATH << 'app/models'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
