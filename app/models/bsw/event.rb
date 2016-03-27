3cdf4f87b95c80e3fb655e5fc7b6ee52ea263e78
Brady Wied <brady@bswtechconsulting.com>
2016-03-27 00:36:50 -0600

    this worked

--- app/models/bsw/event.rb
+++ app/models/bsw/event.rb
@@ -4,8 +4,8 @@ module Bsw
   class Event
     # TODO: Hibernate only finds the attributes if they are set from outside the class (not as ruby @fields)
     def initialize(title, date)
-        @title = title
-        @date = date
+        self.title = title
+        self.date = date
     end
 
     # TODO: Move these out
