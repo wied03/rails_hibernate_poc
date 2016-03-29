0ff3a4edd694e0bb68f3644bc1c3e7f157b1f83e
Brady Wied <brady@bswtechconsulting.com>
2016-03-29 10:26:06 -0600

    This fired, but we were hitting this anyways, somehow still relying on static hibernate config

--- app/mappings/session_factory_fetcher.rb
+++ app/mappings/session_factory_fetcher.rb
@@ -2,9 +2,25 @@ import 'org.hibernate.boot.registry.StandardServiceRegistryBuilder'
 import 'org.hibernate.boot.registry.BootstrapServiceRegistryBuilder'
 import 'org.hibernate.boot.MetadataSources'
 
+ActiveSupport.on_load :hibernate_session_factory do
+    # TODO: This is triggering on any code change
+    puts 'reload!'
+    # if self.respond_to?(:reset_session_factory)
+    #     puts 'resetting session_factory'
+    #     self.reset_session_factory
+    # end
+end
+
 class SessionFactoryFetcher
+    ActiveSupport.run_load_hooks(:hibernate_session_factory, self)
+
+    def self.reset_session_factory
+        @session_factory = nil
+    end
+
     def self.session_factory
         @session_factory ||= begin
+            puts 'rebuilding session factory'
             # TODO: Map ruby types to java ones with some fluent mapping code??
             fields = {
                 id: 'java.lang.Long',
