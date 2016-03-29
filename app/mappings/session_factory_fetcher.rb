d5e38fda3fa4d21e8c6a2e15be867e8c30bae995
Brady Wied <brady@bswtechconsulting.com>
2016-03-29 01:48:05 -0600

    this matches the type better but still doesn't work

--- app/mappings/session_factory_fetcher.rb
+++ app/mappings/session_factory_fetcher.rb
@@ -9,7 +9,7 @@ class SessionFactoryFetcher
             fields = {
                 id: 'java.lang.Long',
                 desc: 'java.lang.String',
-                greetings: 'java.util.List'
+                greetings: 'java.util.Collection'
             }
             # TODO: Find models automatically
             ModelAnnotator.annotate(Bsw::Event, fields)
